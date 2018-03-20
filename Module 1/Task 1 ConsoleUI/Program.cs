ad87edb50cbb1aa128e9dbdb8d9084e84e98f691
сергей слиж <sergeislizh@mail.ru>
2018-03-21 00:55:00 +0300

    add some unit tests

--- Module 1/Task 1 ConsoleUI/Program.cs
+++ Module 1/Task 1 ConsoleUI/Program.cs
@@ -17,10 +17,34 @@ namespace Task_1_ConsoleUI
             fsv.EndNotification += (o, s) => Console.WriteLine(s.Message);
             fsv.FileFindedNotification += (o, s) => Console.WriteLine(s.Message);
             fsv.DirectoryFindedNotification += (o, s) => Console.WriteLine(s.Message);
+            fsv.FilteredFileFindedNotification += (o, s) => Console.WriteLine(s.Message);
+            fsv.FilteredDirectoryFindedNotification += (o, s) => Console.WriteLine(s.Message);
+
             Console.WriteLine("Part1");
             try
             {
-                foreach (var element in fsv.GetFilesAndFolders(@"E:\other"))
+                foreach (var element in fsv.GetFilesAndFoldersSequence(@"E:\other"))
+                {
+                    Console.WriteLine(element);
+                }
+                Console.WriteLine("------------------");
+                Console.WriteLine("GOOOOD");
+            }
+            catch (DirectoryNotFoundException exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+    
+            Console.WriteLine();
+
+            Console.WriteLine("Part 2");
+            try
+            {
+                foreach (var element in fsv.GetFilesAndFoldersSequence(@"E:\other", x => x.Length > 8))
                 {
                     Console.WriteLine(element);
                 }
@@ -37,24 +61,25 @@ namespace Task_1_ConsoleUI
             }
 
             Console.WriteLine();
-            Console.WriteLine("Part 2");
-            try
-            {
-                foreach (var element in fsv.GetFilesAndFolders(@"E:\other", x => x.Length > 8))
-                {
-                    Console.WriteLine(element);
-                }
-                Console.WriteLine("------------------");
-                Console.WriteLine("GOOOOD");
-            }
-            catch (DirectoryNotFoundException exc)
-            {
-                Console.WriteLine(exc.Message);
-            }
-            catch (Exception exc)
-            {
-                Console.WriteLine(exc.Message);
-            }
+
+            //Console.WriteLine("Part 3");
+            //try
+            //{
+            //    foreach (var element in fsv.GetFilesAndFolders(@"E:\other"))
+            //    {
+            //        Console.WriteLine(element);
+            //    }
+            //    Console.WriteLine("------------------");
+            //    Console.WriteLine("GOOOOD");
+            //}
+            //catch (DirectoryNotFoundException exc)
+            //{
+            //    Console.WriteLine(exc.Message);
+            //}
+            //catch (Exception exc)
+            //{
+            //    Console.WriteLine(exc.Message);
+            //}
         }
     }
 }
