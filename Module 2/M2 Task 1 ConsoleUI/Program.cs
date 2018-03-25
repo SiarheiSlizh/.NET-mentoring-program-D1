d8976edf87a2bfc8fe53dbbc8e708fd57c5ed37c
сергей слиж <sergeislizh@mail.ru>
2018-03-26 01:38:41 +0300

    task 1 and task 2 are performed

---
+++ Module 2/M2 Task 1 ConsoleUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M2_Task_1_ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            do
+            {
+                Console.WriteLine("\nEnter string");
+                str = Console.ReadLine();
+                try
+                {
+                    Console.WriteLine(str.Trim()[0]);
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("The string is empty. Try again");
+                }
+                Console.WriteLine("Would you like to continue?\nYes - Press any key\nNo - press escape");
+            } while (Console.ReadKey().Key != ConsoleKey.Escape);
+
+            Console.WriteLine("Finish");
+        }
+    }
+}
---
+++ Module 2/M2 Task 2/StringConverter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace M2_Task_2
+{
+    public static class StringConverter
+    {
+        #region props
+        public static Regex intRegex = new Regex(@"^([\d]|\+[\d]|-[\d])\d*");
+        #endregion
+
+        #region API
+        public static int ToInt(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                throw new ArgumentNullException("String is empty");
+            }
+            
+            if (!intRegex.IsMatch(str))
+            {
+                throw new FormatException("Incorrect format");
+            }
+            
+            int sign = str[0] == '-' ? -1 : 1;
+            int start = sign == 1 && str[0] != '+' ? 0 : 1;
+
+            try
+            {
+                return GetResult(start, str) * sign;
+            }
+            catch (OverflowException)
+            {
+                throw;
+            }
+        }
+
+        public static bool TryToInt(this string str, out int number)
+        {
+            number = 0;
+
+            if (string.IsNullOrWhiteSpace(str) || !intRegex.IsMatch(str))
+            {
+                return false;
+            }
+
+            int sign = str[0] == '-' ? -1 : 1;
+            int start = sign == 1 && str[0] != '+' ? 0 : 1;
+
+            try
+            {
+                number = GetResult(start, str) * sign;
+            }
+            catch (OverflowException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        #endregion
+
+        #region private methods
+        static private int GetResult(int start, string str)
+        {
+            int result = 0;
+
+            for (int i = start; i < str.Length; i++)
+            {
+                result *= result + (str[i] - '0');
+            }
+            return result;
+        }
+        #endregion
+    }
+}
