7da956c6c7b31f0f926c715705f1efa764df574f
сергей слиж <sergeislizh@mail.ru>
2018-05-01 14:02:36 +0300

    add full task 2

--- Module 6/Northwind/Tasks/queriesTask1-2.sql
+++ Module 6/Northwind/Tasks/queriesTask1-2.sql
@@ -129,3 +129,130 @@ FROM Orders;
 
 SELECT COUNT(DISTINCT CustomerID)
 FROM Orders;
+
+-- 2.2.1 
+-- По таблице Orders найти количество заказов с группировкой по годам. В результатах запроса 
+-- надо возвращать две колонки c названиями Year и Total. Написать проверочный запрос, который 
+-- вычисляет количество всех заказов.
+
+SELECT COUNT(OrderID) AS Total, 
+	YEAR(ShippedDate) AS Year
+FROM ORDERS
+GROUP BY YEAR(ShippedDate);
+
+-- 2.2.2 
+-- По таблице Orders найти количество заказов, cделанных каждым продавцом. Заказ для указанного
+-- продавца – это любая запись в таблице Orders, где в колонке EmployeeID задано значение для 
+-- данного продавца. В результатах запроса надо возвращать колонку с именем продавца 
+-- (Должно высвечиваться имя полученное конкатенацией LastName & FirstName. 
+-- Эта строка LastName & FirstName должна быть получена отдельным запросом в колонке основного 
+-- запроса. Также основной запрос должен использовать группировку по EmployeeID.) с названием 
+-- колонки ‘Seller’ и колонку c количеством заказов возвращать с названием 'Amount'. 
+-- Результаты запроса должны быть упорядочены по убыванию количества заказов.
+
+SELECT EmployeeID AS Seller, 
+	(SELECT CONCAT(LastName, ' ', FirstName)
+	FROM Employees
+	WHERE EmployeeID = Orders.EmployeeID),
+	COUNT(EmployeeID) AS Amount
+FROM Orders
+GROUP BY EmployeeID
+ORDER BY Amount DESC;
+
+-- 2.2.3 
+-- По таблице Orders найти количество заказов, сделанных каждым продавцом и для каждого покупателя. 
+-- Необходимо определить это только для заказов, сделанных в 1998 году.
+
+SELECT EmployeeID, CustomerID, COUNT(CustomerID) AS Amount
+FROM Orders
+WHERE YEAR(ShippedDate) = '1998'
+GROUP BY EmployeeID, CustomerID;
+
+-- 2.2.4
+-- Найти покупателей и продавцов, которые живут в одном городе. Если в городе живут только один
+-- или несколько продавцов, или только один или несколько покупателей, то информация о таких 
+-- покупателя и продавцах не должна попадать в результирующий набор. Не использовать конструкцию JOIN.
+
+SELECT ContactName,
+	CONCAT(LastName, ' ', FirstName),
+	C.City
+FROM Customers AS C, Employees AS E
+WHERE C.City = E.City;
+
+-- 2.2.5
+-- Найти всех покупателей, которые живут в одном городе.
+
+SELECT City,
+	COUNT(CompanyName) AS Amount
+FROM Customers
+GROUP BY City
+
+-- 2.2.6
+-- По таблице Employees найти для каждого продавца его руководителя.
+
+SELECT CONCAT(LastName, ' ', FirstName) AS Employee,
+	CASE 
+	WHEN ReportsTo IS NULL
+	THEN
+		'No Header'
+	ELSE
+		(SELECT CONCAT(LastName, ' ', FirstName)
+		FROM Employees AS R
+		WHERE R.EmployeeID = E.ReportsTo)
+	END AS Header
+FROM Employees AS E;
+
+-- 2.3.1
+-- Определить продавцов, которые обслуживают регион 'Western' (таблица Region).
+
+SELECT FirstName, LastName, RegionDescription
+FROM Employees AS E
+JOIN EmployeeTerritories AS ET ON ET.EmployeeID = E.EmployeeID
+JOIN Territories AS T ON T.TerritoryID = ET.TerritoryID 
+JOIN Region AS R ON T.RegionID = R.RegionID AND RegionDescription = 'Western'
+GROUP BY FirstName, LastName, RegionDescription;
+
+-- 2.3.2
+-- Выдать в результатах запроса имена всех заказчиков из таблицы Customers и суммарное количество
+-- их заказов из таблицы Orders. Принять во внимание, что у некоторых заказчиков нет заказов, но 
+-- они также должны быть выведены в результатах запроса. Упорядочить результаты запроса по возрастанию количества заказов.
+
+SELECT C.CompanyName,
+	COUNT(O.OrderID) AS Amount
+FROM Customers AS C
+LEFT JOIN Orders AS O ON O.CustomerID = C.CustomerID
+GROUP BY C.CompanyName
+ORDER BY Amount;
+
+-- 2.4.1
+-- Выдать всех поставщиков (колонка CompanyName в таблице Suppliers), у которых нет хотя бы одного продукта
+-- на складе (UnitsInStock в таблице Products равно 0). Использовать вложенный SELECT для этого запроса с использованием оператора IN.
+
+SELECT CompanyName
+FROM Suppliers
+WHERE SupplierID IN 
+	(SELECT SupplierID
+	FROM Products
+	WHERE UnitsInStock = 0);
+
+-- 2.4.2 
+-- Выдать всех продавцов, которые имеют более 150 заказов. Использовать вложенный SELECT.
+
+SELECT FirstName, LastName, EmployeeID
+FROM Employees
+WHERE EmployeeID IN 
+	(SELECT EmployeeID
+	FROM Orders
+	GROUP BY EmployeeID
+	HAVING COUNT(EmployeeID) > 150);
+
+-- 2.4.3 
+-- Выдать всех заказчиков (таблица Customers), которые не имеют ни одного заказа 
+-- (подзапрос по таблице Orders). Использовать оператор EXISTS.
+
+SELECT CompanyName
+FROM Customers AS C
+WHERE NOT EXISTS
+	(SELECT CustomerID
+	FROM Orders AS O
+	WHERE O.CustomerID = C.CustomerID);
